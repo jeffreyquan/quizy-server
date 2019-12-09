d13b4224f944d8f7e8ee179385d97d782efbc8aa
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-10 01:01:55 +1100

    started populating player model

--- api/models/playerModel.js
+++ api/models/playerModel.js
@@ -2,5 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const PlayerSchema = new Schema({
+  nickname: {
+    type: String,
+    required: 'Nickname cannot be blank'
+  },
+  score: {
+    type: Number
+  },
   
 })
