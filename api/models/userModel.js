521612d2987b40d17a439ad0965f3454d7ad949c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-12 10:36:15 +1100

    created user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,5 +2,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  
-})
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
+}, { collection: 'user' });
+
+module.exports = mongoose.model('User', UserSchema);
